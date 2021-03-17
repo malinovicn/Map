b173ed0a0df7f75909c241e234275f2f93a6c496
Nikola Malinovic <nikola.malinovic@netcetera.com>
2021-03-17 12:15:49 +0100

    Initial Commit

---
+++ Map/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Map
+//
+//  Created by Nikola Malinovic on 17.03.21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Map/MapApp.swift
@@ -0,0 +1,17 @@
+//
+//  MapApp.swift
+//  Map
+//
+//  Created by Nikola Malinovic on 17.03.21.
+//
+
+import SwiftUI
+
+@main
+struct MapApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
