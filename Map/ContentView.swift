4781febbfceacd66141b3ff4348bf00d335d8592
Nikola Malinovic <nikola.malinovic@netcetera.com>
2021-03-20 14:00:46 +0100

    Commit initial setup of mapbox

--- Map/ContentView.swift
+++ Map/ContentView.swift
@@ -6,11 +6,16 @@
 //
 
 import SwiftUI
+import Mapbox
 
 struct ContentView: View {
+
+    @State var annotations: [MGLPointAnnotation] = [
+        MGLPointAnnotation(title: "Mapbox", coordinate: .init(latitude: 37.791434, longitude: -122.396267))
+    ]
+
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        MapView(annotations: $annotations).centerCoordinate(.init(latitude: 37.791293, longitude: -122.396324)).zoomLevel(16)
     }
 }
 
